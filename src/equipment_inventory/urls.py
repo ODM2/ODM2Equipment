9d46e84dac3f1df06a490fa6f716c5f2fb465931
Craig Blackburn <craig.blackburn@usu.edu>
2018-07-30 18:43:36 -0600

    fixed merge issue with development

--- src/equipment_inventory/urls.py
+++ src/equipment_inventory/urls.py
@@ -43,5 +43,6 @@ urlpatterns = [
     path('equipments', EquipmentListView.as_view(), name='equipment-list'),
     path('equipment-details/<int:pk>', EquipmentDetailView.as_view(), name='equipment-details'),
     path('equipment-model-list', EquipmentModelListView.as_view(), name='equipment-model-list'),
-    path('equipment-model-details/<int:pk>', EquipmentModelDetailView.as_view(), name='equipment-model-details')
+    path('equipment-model-details/<int:pk>', EquipmentModelDetailView.as_view(), name='equipment-model-details'),
+    path('instrument-output-variables', InstrumentOutputVariablesListView.as_view(), name='instrument-output-variables'),
 ]
